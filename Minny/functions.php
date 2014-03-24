05dd3a853620e353cde6608be94ed86a365cad19
Calvin Koepke <hi@clvnk.com>
2014-03-24 16:46:29 -0600

    Added files.

--- Minny/functions.php
+++ Minny/functions.php
@@ -8,22 +8,12 @@ add_theme_support( 'html5' );
 //* Force full-width-content layout setting
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
-//* Unregister content/sidebar layout setting
+//* Unregister layout settings
 genesis_unregister_layout( 'content-sidebar' );
- 
-//* Unregister sidebar/content layout setting
 genesis_unregister_layout( 'sidebar-content' );
- 
-//* Unregister content/sidebar/sidebar layout setting
 genesis_unregister_layout( 'content-sidebar-sidebar' );
- 
-//* Unregister sidebar/sidebar/content layout setting
 genesis_unregister_layout( 'sidebar-sidebar-content' );
- 
-//* Unregister sidebar/content/sidebar layout setting
 genesis_unregister_layout( 'sidebar-content-sidebar' );
-
-//* Remove Genesis Layout Settings
 remove_theme_support( 'genesis-inpost-layouts' );
 
 //* Disable the superfish script
@@ -39,10 +29,20 @@ function child_viewport_meta_tag() {
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
 
+//* Remove Post Meta
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+
+//* Customize the entry meta in the entry header (requires HTML5 theme support)
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date] in [post_categories before=""] with [post_comments zero="0 Comments" one="1 Comment" more="% Comments"] [post_edit]';
+	return $post_info;
+}
+
 //* Code to Display Featured Image on top of the post */
 add_action( 'genesis_before_entry', 'featured_post_image', 8 );
 function featured_post_image() {
-	if ( has_post_thumbnail() ) {
+	if ( has_post_thumbnail() && is_singular( 'post' ) ) {
 		echo '<a href="',
 		the_permalink(),
 		'" title="',
@@ -51,38 +51,97 @@ function featured_post_image() {
 		'<div class="featured-img">';
 		the_post_thumbnail('post-image');
 		echo '</div></a>';
+		add_action( 'genesis_entry_footer', 'genesis_post_meta' );
 	}
 }
 
-add_action( 'wp_enqueue_scripts', 'script_managment', 99);
-/**
- * Change the location of jQuery.
- *
- * @author Greg Rickaby
- * @since 1.0.0
- */
-function script_managment() {
-	  wp_deregister_script( 'jquery' );
-	  wp_deregister_script( 'jquery-ui' );
-	  wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' );
-	  wp_register_script( 'jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js' );
-	  wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', array( 'jquery' ), '4.0', false );
-	  wp_enqueue_script( 'jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js', array( 'jquery' ), '1.8.16' );
+//* Filter open graph tags to use Genesis doctitle and meta description instead
+//* @author Brian Gardner
+add_filter( 'jetpack_open_graph_tags', 'minny_jetpack_open_graph_tags_filter' );
+function minny_jetpack_open_graph_tags_filter( $tags ) {
+
+	// Do nothing if not a single post
+	if ( ! is_singular() )
+		return $tags;
+
+	// Pull from custom fields
+	$title = genesis_get_custom_field( '_genesis_title' );
+	$description = genesis_get_custom_field( '_genesis_description' );
+
+	// Set new values for title and description
+	$tags['og:title'] = $title ? $title : $tags['og:title'];
+	$tags['og:description']	= $description ? $description : $tags['og:description'];
+
+	return $tags;
+
 }
 
-//* Register after post widget area
+//* Replace Twitter OG handle to @calvinkoepke
+//* @author Brian Gardner
+add_filter( 'jetpack_open_graph_tags', 'minny_jetpack_twitter_username', 11 );
+function minny_jetpack_twitter_username( $og_tags ) {
+
+	$og_tags['twitter:site'] = '@calvinkoepke';
+	return $og_tags;
+
+}
+
+//* Customize Read More Link
+function new_excerpt_more( $more ) {
+	return '... <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+//* Enqueue Theme Styles & Imports
+function minny_styles() {
+	// Enqueue Google Fonts (Better performance when executing as a stylesheet, instead of @import)
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic' );
+
+	// Add Custom Scripts
+	// wp_enqueue_script( 'minnyScripts', get_stylesheet_directory_uri() . '/js/minnyScripts.js', array('jquery'));
+}
+add_action( 'wp_enqueue_scripts', 'minny_styles', 99);
+
+//* Remove Unecessary Sidebars
+unregister_sidebar( 'sidebar' );
+unregister_sidebar( 'sidebar-alt' );
+unregister_sidebar( 'header-right' );
+
+
+//* Register Widget Areas
 genesis_register_sidebar( array(
 	'id'            => 'social-networks',
 	'name'          => __( 'Social Networks', 'profile' ),
 	'description'   => __( 'This area is meant to be used with the Simple Social Icons plugin.', 'profile' ),
 ) );
 
+genesis_register_sidebar( array(
+	'id'          => 'email-callout',
+	'name'        => __( 'Email Callout', 'minny' ),
+	'description' => __( 'This is the email callout section after the entry.', 'minny' ),
+) );
+
+//* Hooks above content
 add_action( 'genesis_before_header', 'social_networks' );
 function social_networks() {
 	genesis_widget_area( 'social-networks', array(
 		'before' => '<div class="before-header widget-area">',
 		'after' => '</div>',
 	) );
+}
+ 
+//* Hooks after-entry widget area to single posts
+add_action( 'genesis_entry_footer', 'minny_after_entry_widget'  ); 
+function minny_after_entry_widget() {
+ 
+    if ( ! is_singular( 'post' ) )
+    	return;
+ 
+    genesis_widget_area( 'email-callout', array(
+		'before' => '<div class="after-entry widget-area" id="email-callout"><div class="wrap">',
+		'after'  => '</div></div>',
+    ) );
+ 
 }
 
 //* Add support for post formats
@@ -113,7 +172,7 @@ function sp_footer_creds_text() {
 	echo '<div class="creds"><p>';
 	echo 'Copyright &copy; ';
 	echo date('Y');
-	echo ' &middot; <a href="http://twitter.com/calvinkoepke">Crafted by Calvin Koepke</a> &middot; Built on the <a href="http://www.shareasale.com/r.cfm?b=460184&u=884099&m=28169&urllink=&afftrack=" title="Genesis Framework">Genesis Framework</a>';
+	echo ' &middot; <a href="http://demo.calvinkoepke.com/minny/" target="_blank" title="Minny Theme">Minny Theme</a> &middot; Inspired by <a href="http://matmm.me/typesense" target="_blank" follow="nofollow">Typesense</a> &middot; Built on the <a target="_blank" href="http://calvinkoepke.com/go/genesis-framework/" title="Genesis Framework">Genesis Framework</a> &middot; <a href="http://plus.google.com/+CalvinKoepke" rel="author" target="_blank">Google+</a>';
 	echo '</p></div>';
 }
 
