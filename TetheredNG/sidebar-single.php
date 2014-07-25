b332efb9e0b69052480ff3e31449a80a3de00049
Calvin Koepke <hi@clvnk.com>
2014-07-25 01:08:01 -0600

    Moved Tethered to it's own directory.
    Tethered was not a Genesis Child theme, so keeping it here didn’t make
    sense.

--- TetheredNG/sidebar-single.php
+++
@@ -1,6 +0,0 @@
-<section id="sidebar" class="span4 sidebar-single" role="complementary">
-		<?php if ( !dynamic_sidebar() ) : ?>
-			<?php dynamic_sidebar( 'right-sidebar' ); ?>
-		<?php endif; ?>
-	</section>
-</div>
--- TetheredNG/sidebar.php
+++
@@ -1,6 +0,0 @@
-<section id="sidebar" class="span4" role="complementary">
-		<?php if ( !dynamic_sidebar() ) : ?>
-			<?php dynamic_sidebar( 'right-sidebar' ); ?>
-		<?php endif; ?>
-	</section>
-</div>
